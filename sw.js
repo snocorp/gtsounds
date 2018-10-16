cfd7d69bdd7d4e23f6284fc92aad8c3506a5c27a
Dave Sewell <snocorp@gmail.com>
2018-10-15 23:50:03 -0400

    Initial commit

---
+++ sw.js
@@ -0,0 +1,40 @@
+const cacheName = 'gtsounds-1';
+
+self.addEventListener('install', function(event) {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('caching files');
+      return cache.addAll(
+        [
+          '/manifest.webmanifest',
+          '/audio.ogg',
+          '/index.html',
+          '/'
+        ]
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      // TODO 4 - Add fetched files to the cache
+
+    }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
+    })
+  );
+});
